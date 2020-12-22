0523678cdd544303dd9eda3963d29dd067600ea5
eric2fitting <46014461+eric2fitting@users.noreply.github.com>
2020-12-22 20:56:48 +0800

    change

--- src/main/java/com/prv/pojo/UserTest.java
+++ src/main/java/com/prv/pojo/UserTest.java
@@ -14,24 +14,6 @@ import lombok.Data;
 public class UserTest {
 	private Long id;
 	private String name;
-	private Integer age2;
-//	public Long getId() {
-//		return id;
-//	}
-//	public void setId(Long id) {
-//		this.id = id;
-//	}
-//	public String getName() {
-//		return name;
-//	}
-//	public void setName(String name) {
-//		this.name = name;
-//	}
-//	public Integer getAge() {
-//		return age;
-//	}
-//	public void setAge(Integer age) {
-//		this.age = age;
-//	}
-	
+	private String hobby;
+	private Integer age2;	
 }
