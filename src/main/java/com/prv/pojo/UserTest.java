bcac51fde97c4fc5a55804cba9037a8a3cbac810
Administrator <270407494@qq.com>
2020-12-22 21:41:52 +0800

    主分支修改

--- src/main/java/com/prv/pojo/UserTest.java
+++ src/main/java/com/prv/pojo/UserTest.java
@@ -14,24 +14,24 @@ import lombok.Data;
 public class UserTest {
 	private Long id;
 	private String name;
-	private Integer age1;
-//	public Long getId() {
-//		return id;
-//	}
-//	public void setId(Long id) {
-//		this.id = id;
-//	}
-//	public String getName() {
-//		return name;
-//	}
-//	public void setName(String name) {
-//		this.name = name;
-//	}
-//	public Integer getAge() {
-//		return age;
-//	}
-//	public void setAge(Integer age) {
-//		this.age = age;
-//	}
+	private Integer age;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
 	
 }
